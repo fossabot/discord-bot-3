5b3d1db5568646e8c6da28b7c507afba7be0b2a9
Petr Bulant <Petr.bulant@polarmoment.co.uk>
2020-07-22 18:10:26 +0200

    Better formatting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require("./config.json");
 const Snoowrap = require("snoowrap");
 const requireAll = require("require-all");
 // eslint-disable-next-line no-unused-vars
-const colors = require("colors");
+require("colors");
 
 var dbl;
 
@@ -72,7 +72,7 @@ client.config = config;
             return (a[1] > b[1] && -1) || (a[1] === b[1] ? 0 : 1);
         }));
         for(const [group, length] of groups) {
-            console.log(`[LOAD] Found \u001b[37;1m${length.toString().padStart(2, " ")}\u001b[0m commands in \u001b[36m${group.id}\u001b[0m`);
+            console.log(`[LOAD] Found ${length.toString().padStart(2, " ").bold} commands in ${group.id.toString().cyan}`);
         }
 
         const ready = [
