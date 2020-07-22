34dc7c512f9debee89205c09e1e906ad4b082b91
Petr Bulant <Petr.bulant@polarmoment.co.uk>
2020-07-22 18:35:49 +0200

    Implement reloading structures

---
+++ cmd/special/reloadStructures.js
@@ -0,0 +1,20 @@
+const commando = require("@iceprod/discord.js-commando");
+const path = require("path");
+
+module.exports = class reloadStructures extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "reloadStructures",
+            memberName: "reloadStructures",
+            group: "special",
+            ownerOnly: true,
+            description: "Reloads structures - useful when structures were updated during update",
+            args: []
+        });
+    }
+
+    run(msg) {
+        delete require.cache[path.join(__dirname, "../../structures/index.js")];
+        require("../../structures/index");
+    }
+};
--- structures/index.js
+++ structures/index.js
@@ -1,3 +1,9 @@
+const path = require("path");
+
+delete require.cache[path.join(__dirname, "guild.js")];
+delete require.cache[path.join(__dirname, "member.js")];
+delete require.cache[path.join(__dirname, "textChannel.js")];
+delete require.cache[path.join(__dirname, "user.js")];
 require("./guild");
 require("./member");
 require("./textChannel");
