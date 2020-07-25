a1877ebaf26cf332b1596689cf5e4b87a1ee495f
Noah Manscher <noahmanscher@gmail.com>
2020-07-25 13:21:38 +0200

    Update status.js to support variables

--- cmd/special/status.js
+++ cmd/special/status.js
@@ -1,4 +1,5 @@
 const commando = require("@iceprod/discord.js-commando");
+const { shortNumber } = require("../../utils");
 
 module.exports = class Status extends commando.Command {
     constructor(client) {
@@ -26,12 +27,35 @@ module.exports = class Status extends commando.Command {
     }
 
     async run(msg, cmd) {
+        let done = "Done!";
+
+        if(cmd.name.match(/\{\{[\w.]*?\}\}/g)) {
+            var users = 0;
+            var guilds = 0;
+            for(const guild of this.client.guilds.cache) {
+                guilds++;
+                users += guild[1].memberCount;
+            }
+        }
+
+        const name = cmd.name
+            .replace(/\{\{users\}\}/gi, () => {
+                done += ("\nUsers: " + users);
+                return shortNumber(users);
+            })
+            .replace(/\{\{(guilds|servers)\}\}/gi, () => {
+                done += ("\nGuilds: " + guilds);
+                return shortNumber(guilds);
+            });
+
         const status = {
-            name: cmd.name,
+            name: name,
             type: cmd.type.toUpperCase()
         };
+
         await msg.client.provider.set("global", "status", status);
         msg.client.user.setActivity(status.name, { type: status.type });
-        msg.say("Done!");
+
+        msg.say(done);
     }
 };
