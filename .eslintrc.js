51c13b7c363287214b1864087168b68538215f6d
Petr Bulant <Petr.bulant@polarmoment.co.uk>
2020-07-26 14:24:58 +0200

    Format and fix ecma version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,75 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: [
-    'standard'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    BigInt: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  rules: {
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "space-before-function-paren": ["error", {
-      anonymous: "never",
-      named: "never",
-      asyncArrow: "always"
-    }],
-    indent: ["error", 4, {
-      SwitchCase: 1
-    }],
-    "no-case-declarations": ["off"],
-    "no-async-promise-executor": ["off"],
-    "keyword-spacing": ["error", {
-      before: false,
-      after: false,
-      overrides: {
-        of: {
-          after: true
-        },
-        var: {
-          after: true
-        },
-        else: {
-          before: true,
-          after: true
-        },
-        return: {
-          before: true,
-          after: true
-        },
-        finally: {
-          before: true,
-          after: true
-        },
-        try: {
-          after: true
-        },
-        catch: {
-          before: true
-        },
-        const: {
-          after: true
-        },
-        case: {
-          after: true
-        },
-        continue: {
-          before: true
-        },
-        of: {
-          before: true
-        }
-      }
-    }]
-  }
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: [
+        'standard'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        BigInt: 'readonly'
+    },
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    parser: "babel-eslint",
+    rules: {
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "space-before-function-paren": ["error", {
+            anonymous: "never",
+            named: "never",
+            asyncArrow: "always"
+        }],
+        indent: ["error", 4, {
+            SwitchCase: 1
+        }],
+        "no-case-declarations": ["off"],
+        "no-async-promise-executor": ["off"],
+        "keyword-spacing": ["error", {
+            before: false,
+            after: false,
+            overrides: {
+                of: {
+                    after: true
+                },
+                var: {
+                    after: true
+                },
+                else: {
+                    before: true,
+                    after: true
+                },
+                return: {
+                    before: true,
+                    after: true
+                },
+                finally: {
+                    before: true,
+                    after: true
+                },
+                try: {
+                    after: true
+                },
+                catch: {
+                    before: true
+                },
+                const: {
+                    after: true
+                },
+                case: {
+                    after: true
+                },
+                continue: {
+                    before: true
+                },
+                of: {
+                    before: true
+                }
+            }
+        }]
+    }
 }
